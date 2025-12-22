9665b376fcd2974540f51449cde123e692310bb6
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-22 23:55:02 +0200

    adding Q6

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -81,3 +81,28 @@ function summation(number) {
 }
 console.log(summation(5));
 console.log(summation(8));
+
+//Q6
+// Ex2.6 - Years and Centuries
+// The first century spans from the year 1 up to and including the year 100, The second - from the
+// year 101 up to and including the year 200, etc.
+// Task :
+// Given a year, return the century it is in.
+// Input , Output Examples ::
+// centuryFromYear(1705) returns (18)
+// centuryFromYear( 1900) returns (19)
+// centuryFromYear(1601) returns (17)
+// centuryFromYear(2000) returns (20)
+
+function century(year) {
+
+    if (year % 100 == 0) {
+        return year / 100;
+    }
+    else {
+        return parseInt(year / 100) + 1;
+    }
+}
+console.log(century(2000));
+console.log(century(1601));
+console.log(century(1));
