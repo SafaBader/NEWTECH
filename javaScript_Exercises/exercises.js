3639722b50a829d350a2ccf66e05dd8f245b8248
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-23 00:04:25 +0200

    adding Q9

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -168,3 +168,14 @@ console.log(nbYear(1000, 2, 50, 1200));
 console.log(nbYear(1500000, 2.5, 10000, 2000000));
 console.log(nbYear(1500, 5, 100, 5000));
 
+//Q9
+// Ex5.1 - trimming string
+// It's pretty straightforward. Your goal is to create a function that removes the first and last
+// characters of a string. You're given one parameter, the original string. You don't have to worry
+// with strings with less than two characters.
+
+function removeFirstLast(str) {
+    return str.slice(1, -1);
+}
+console.log(removeFirstLast("safa"));
+
