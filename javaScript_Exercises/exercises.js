e46301f67b30f4f02da769dd9e22f0e46228f3dd
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-22 23:52:37 +0200

    adding Q5

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -61,3 +61,23 @@ function findNextSquare(number) {
 }
 console.log(findNextSquare(9));
 console.log(findNextSquare(114));
+
+//Q5
+// Ex2.5 - Summation
+// Write a program that finds the summation of every number from 1 to num. The number will
+// always be a positive integer greater than 0.
+// For example:
+// summation(2) -> 3
+// 1 + 2
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+function summation(number) {
+    let sum = 0;
+    for (let i = 0; i <= number; i++) {
+        sum += i;
+    }
+    return sum;
+}
+console.log(summation(5));
+console.log(summation(8));
