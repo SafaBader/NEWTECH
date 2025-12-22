b227df1ff5d2d610799ca542256c04ef68eeaca9
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-23 00:33:15 +0200

    adding Q10

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -179,3 +179,17 @@ function removeFirstLast(str) {
 }
 console.log(removeFirstLast("safa"));
 
+// Q10
+// Ex5.2 - String Repeat
+// Write a function called repeat_str which repeats the given string src exactly count times.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function stringReat(repeatNum, string) {
+    let repeatStr = string;
+    for (let i = 1; i < repeatNum; i++) {
+        string += repeatStr;
+    }
+    return string;
+}
+console.log(stringReat(2, "safa"));
