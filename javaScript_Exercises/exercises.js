a93d2cab3e2a14001e2b765115ceb024461b7743
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-22 23:34:06 +0200

    adding Q2

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -1,4 +1,5 @@
-//Q1:Ex1.1 - Yes or No
+//Q1
+// Ex1.1 - Yes or No
 // Complete the method that takes a boolean value
 //  and return a "Yes" string for true, 
 // or a "No" string for false.
@@ -8,3 +9,16 @@ function isBoolean(bool) {
 }
 console.log(isBoolean(true));
 console.log(isBoolean(false));
+
+//Q2
+// Ex2.1 - Sum of lowest numbers
+// Create a function that returns the sum of the two lowest positive numbers given an array of
+// minimum 4 positive integers. No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumLowest2Num(array) {
+    array.sort((a, b) => a - b);
+    return array[0] + array[1];
+}
+console.log(sumLowest2Num([500, 20, 3, 60]));
