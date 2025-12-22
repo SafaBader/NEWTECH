743f2f24d5ffb231c8c7f43abd284546f6e92805
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-22 23:29:20 +0200

    adding Q1

---
+++ javaScript_Exercises/exercises.js
@@ -0,0 +1,10 @@
+//Q1:Ex1.1 - Yes or No
+// Complete the method that takes a boolean value
+//  and return a "Yes" string for true, 
+// or a "No" string for false.
+
+function isBoolean(bool) {
+    return bool ? "yes" : "no";
+}
+console.log(isBoolean(true));
+console.log(isBoolean(false));
