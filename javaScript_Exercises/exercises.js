2ddc6af15fa4fe49fb3a7f74954956943e63d42a
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-23 17:18:43 +0200

    adding Q18

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -341,3 +341,27 @@ function longestWord(str) {
 }
 
 console.log(longestWord("safasafasafa ba baderrr"));
+
+//Q18
+// Ex6.1 - Mumbling
+// This time no story, no theory. The examples below show you how to write function
+// accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeeezzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-VWw-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z anda. .z.
+
+function accum(str) {
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        let newStr = str[i].toUpperCase() + str[i].toLowerCase().repeat(i);
+        result.push(newStr);
+    }
+    return result.join("-");
+}
+console.log(accum("abcd"));
+console.log(accum("RqaEzty"));
+console.log(accum("cwAt"));
+
+
