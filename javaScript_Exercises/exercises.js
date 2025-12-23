8ab412619bfa36ad2b3311bc46c50a59bb0ed90d
SafaBader <117042247+SafaBader@users.noreply.github.com>
2025-12-23 17:39:35 +0200

    adding Q19

--- javaScript_Exercises/exercises.js
+++ javaScript_Exercises/exercises.js
@@ -364,4 +364,45 @@ console.log(accum("abcd"));
 console.log(accum("RqaEzty"));
 console.log(accum("cwAt"));
 
+//Q19
+// Ex6.2 - Counting Duplicates
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and
+// numeric digits that occur more than once in the input string. The input string can be assumed to
+// contain only alphabets (both uppercase and lowercase) and numeric digits.
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 #'a’ and 'b'
+// "aabBcde" -> 2 #'a' occurs twice and 'b' twice (b° and “B’)
+// "indivisibility" -> 1#'i' occurs six times
+// "Indivisibilities" -> 2 #'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 #'a' and '1'
+// "ABBA" -> 2 #'A' and 'B' each occur twice
+
+function cntDublicate(str) {
+    str = str.toLowerCase();
+    let cnt = 0;
+    const freq = {};
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        freq[char] = (freq[char] || 0) + 1;
+    }
+    for (let char in freq) {
+        if (freq[char] > 1) {
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+console.log(cntDublicate("abcde"));
+console.log(cntDublicate("aabbcde"));
+console.log(cntDublicate("aabBcde"));
+console.log(cntDublicate("indivisibility"));
+console.log(cntDublicate("Indivisibilities"));
+console.log(cntDublicate("aA11"));
+console.log(cntDublicate("ABBA"));
+
+
+
 
